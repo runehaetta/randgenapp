bd5b22f73c799e01f1473a0c2ece7dcf917c9bca
Rune Haetta <rune@leanyard.com>
2017-10-23 17:35:57 +0200

    Initial commit

---
+++ RandGenApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandGenApp
+//
+//  Created by Rune Haetta on 23/10/2017.
+//  Copyright © 2017 Leanyard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
