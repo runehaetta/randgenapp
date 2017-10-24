1139a08d52c57b3db098218ed7d7a6d63220797b
Rune Haetta <rune@leanyard.com>
2017-10-24 09:35:37 +0200

    Display coordinate updates

--- RandGenApp/ViewController.swift
+++ RandGenApp/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UITouchViewDelegate {
 
-    func touchesMoved(_ location: CGPoint) {
-        print("touchesMoved", location)
-    }
-    
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var touchView: UITouchView!
+    
+    func touchesMoved(_ location: CGPoint) {
+        let str = "\(location.x),\(location.y)"
+        
+        self.passwordLabel.text = str
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
