c1e9a0034d1048ae198d8e34f377562ac03fee73
Rune Haetta <rune@leanyard.com>
2017-11-07 18:39:05 +0100

    Move controller stub

---
+++ RandGenApp/MoveViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MoveViewController.swift
+//  RandGenApp
+//
+//  Created by Rune Haetta on 07/11/2017.
+//  Copyright © 2017 Leanyard. All rights reserved.
+//
+
+import UIKit
+
+class MoveViewController: UIViewController {
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBAction func startAction(_ sender: Any) {
+    }
+    @IBAction func stopAction(_ sender: Any) {
+    }
+    @IBAction func clearAction(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
